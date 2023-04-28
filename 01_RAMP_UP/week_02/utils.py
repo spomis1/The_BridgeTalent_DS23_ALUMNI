d82270f415f8c0edae07260895bd70f761f0ab74
marcusRB <mrussorb@gmail.com>
2023-04-28 20:04:46 +0200

    update w2d3

--- 01_RAMP_UP/week_02/utils.py
+++ 01_RAMP_UP/week_02/utils.py
@@ -11,6 +11,7 @@ description:
 
 def get_avg_age(birth_year):
     """
+    Docstring
     Parameters:
     ----------
         birth_year list int
@@ -25,22 +26,30 @@ def get_avg_age(birth_year):
     '''
     import numpy as np
     from datetime import datetime
-    this_year = datetime.today().year # extrae el anyo
     
-    # Calculamos las edades
-    ages = []
-    for anyo in birth_year:
-        age = this_year - anyo
-        ages.append(age)
-    print("***"*20)
-    print(ages) # mostramos el resultado parcial en pantalla
+    try:
+        this_year = datetime.today().year # extrae el anyo
+        
+        # Calculamos las edades
+        ages = []
+        for anyo in birth_year:
+            age = this_year - anyo
+            ages.append(age)
+        print("***"*20)
+        print(ages) # mostramos el resultado parcial en pantalla
 
-    # calculamos el promedio
-    age_sum = sum(ages)
-    n_people = len(ages)
-    age_mean = age_sum / n_people
+        # calculamos el promedio
+        age_sum = sum(ages)
+        n_people = len(ages)
+        age_mean = age_sum / n_people
+        age_max = max(ages)
+        print("/#"*20)
 
-    print("/#"*20)
-
-    # print(ages)
-    return print(np.round(age_mean, 1))
+        print(np.round(age_mean, 1))
+        return np.round(age_mean, 1), age_max
+    
+    except Exception as e:
+        print(f"El error encontrado es {e}")
+        
+    finally:
+        pass
