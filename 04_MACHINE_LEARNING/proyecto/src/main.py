8cf5b4c15570c3aa4afd7f2317cf57400777b0dd
marcusRB <mrussorb@gmail.com>
2023-09-30 13:50:49 +0200

    update_machine_learning

---
+++ 04_MACHINE_LEARNING/proyecto/src/main.py
@@ -0,0 +1,45 @@
+'''
+Name: main.py
+Version: 1.0
+Description:
+Release_date:
+Updated_date:
+Author:
+
+'''
+import os, sys
+import logging
+import pandas as pd
+import utils.custom_functions as f
+import sqlite3
+
+## Cargando las variables de entorno
+f.get_args()
+
+environment = f.options.env
+print(environment)
+
+data_source = f.options.data_source
+print(data_source)
+
+db_table_name = f.options.dbtable
+
+# Configuramos el formato del logger
+FORMAT = '%(asctime)-15s - %(message)s'
+logging.basicConfig(format=FORMAT
+                    ,level=logging.INFO
+                    ,filemode='w'
+                    )
+logger = logging.getLogger("main.py")
+logger.setLevel("INFO")
+
+def main():
+    db = f.TheBridgeDatabase(env=environment)
+    db.create_database_sqlite()
+    tables = db.read_table(table_name=db_table_name)
+    print(tables)
+
+if __name__ == '__main__':
+    logger.info("--- Script start ---")
+    main()
+    logger.info("--- Script end ---")
